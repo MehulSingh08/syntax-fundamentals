f9410388cd3e051436b05a792f5ce5cbbe82facc
Metagrammer <113834047+Metagrammer@users.noreply.github.com>
2025-05-18 18:39:05 +0530

    name recursion

---
+++ recursion/name.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class name{
+    static int count = 0;
+    static void printname(){
+        if(count>10){
+            return;
+        }
+        System.out.println("Mehul Singh");
+        count++;
+        printname();
+    }
+    public static void main(String[] args){
+        printname();
+    }
+}
