5728c3f9168382bc073dde2eaaa88dd34dfb492a
Metagrammer <113834047+Metagrammer@users.noreply.github.com>
2025-05-28 17:33:22 +0530

    Insert

---
+++ trees/preorder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class preorder
+{
+    class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+    }
+    static void preorder(node)
+    {
+
+    }
+}
